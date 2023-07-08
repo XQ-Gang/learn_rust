5e94db204be818c22217b1f12e5a56c82d6cbcf5
XQ-Gang <3035686788@qq.com>
2023-07-09 00:59:02 +0800

    feat: control flow

---
+++ _03_common_concept/src/control_flow.rs
@@ -0,0 +1,52 @@
+fn main() {
+    // if
+    let condition = true;
+    let number = if condition { 5 } else { 6 };
+    println!("The value of number is: {number}");
+
+    // loop
+    let mut counter = 0;
+    let result = loop {
+        counter += 1;
+        if counter == 10 {
+            break counter * 2;
+        }
+    };
+    println!("The value of result is: {result}");
+
+    // loops
+    let mut count = 0;
+    'counting_up: loop {
+        println!("count = {count}");
+        let mut remaining = 10;
+        loop {
+            println!("remaining = {remaining}");
+            if remaining == 9 {
+                break;
+            }
+            if count == 2 {
+                break 'counting_up;
+            }
+            remaining -= 1;
+        }
+        count += 1;
+    }
+    println!("End count = {count}");
+
+    // while
+    let mut number = 3;
+    while number != 0 {
+        println!("{number}!");
+        number -= 1;
+    }
+    println!("LIFTOFF!!!");
+
+    // for
+    for element in [10, 20, 30, 40, 50] {
+        println!("the value is: {element}");
+    }
+    for number in (1..4).rev() {
+        println!("{number}!");
+    }
+    println!("LIFTOFF!!!");
+}
