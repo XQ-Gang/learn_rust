7e4d0058e53a652652f584ad6cdb0efe1f96d899
XQ-Gang <3035686788@qq.com>
2023-07-23 23:12:10 +0800

    style: code format

--- _02_guessing_game/src/main.rs
+++ _02_guessing_game/src/main.rs
@@ -12,9 +12,9 @@ fn main() {
     loop {
         println!("Please input your guess.");
         let mut guess = String::new();
-        io::stdin().
-            read_line(&mut guess).
-            expect("Failed to read line");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
         let guess: u32 = match guess.trim().parse() {
             Ok(num) => num,
             Err(_) => continue,
