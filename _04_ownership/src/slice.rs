68681601d2fa76182a9067fad673f07d367ea369
XQ-Gang <3035686788@qq.com>
2023-07-25 03:53:33 +0800

    feat: slice

---
+++ _04_ownership/src/slice.rs
@@ -0,0 +1,41 @@
+fn first_word(s: &str) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+
+    &s[..]
+}
+
+fn main() {
+    let my_string = String::from("hello world");
+
+    // `first_word` 适用于 `String`（的 slice），部分或全部
+    let word = first_word(&my_string[0..6]);
+    println!("the first word is: {}", word);
+
+    let word = first_word(&my_string[..]);
+    println!("the first word is: {}", word);
+
+    // `first_word` 也适用于 `String` 的引用，
+    // 这等价于整个 `String` 的 slice
+    let word = first_word(&my_string);
+    println!("the first word is: {}", word);
+
+    let my_string_literal = "hello world";
+
+    // `first_word` 适用于字符串字面值，部分或全部
+    let word = first_word(&my_string_literal[0..6]);
+    println!("the first word is: {}", word);
+
+    let word = first_word(&my_string_literal[..]);
+    println!("the first word is: {}", word);
+
+    // 因为字符串字面值已经 **是** 字符串 slice 了，
+    // 这也是适用的，无需 slice 语法！
+    let word = first_word(my_string_literal);
+    println!("the first word is: {}", word);
+}
